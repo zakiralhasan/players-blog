57d5564bc4dbec7b493a6fa18a7e32d895e65943
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-09-01 19:26:19 +0600

    add no image section

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-// const url = `https://www.thesportsdb.com/api/v1/json/{APIKEY}/searchplayers.php?t={TeamName}&p={Playername}`
-// const url = `https://www.thesportsdb.com/api/v1/json/{APIKEY}/searchplayers.php?t=${Playername}`
-
+// below line for view all button 
+const viewAllbutton = document.getElementById('view-all-btn');
+// below line for no image found message
+const noImageMsg = `./images/no-image.png`;
+/* 
+---------------------main section start ----------------------------------------
+*/
+// this function for get the data from API 
 function displayPlayer(name, showAllValues){
     fetch(`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${name}`)
     .then(res => res.json())
     .then(data => setPlayerData(data.player, showAllValues))
 };
-// let count = 0;
-
+// this function for manage the data which gets from the API
 function setPlayerData(players, showAllValues){
     const playerDetailsContainer = document.getElementById('player-details-container');
     playerDetailsContainer.textContent = '';
-
-    console.log(showAllValues)
-    console.log(players.length)
-
-    if(players.length > 9 && showAllValues){
+    // this condition section works for data slicing and set class for view all button
+    if(players.length > 9 && showAllValues){ 
         players = players;
-        console.log('unslice')
-        console.log(players.length)
+        viewAllbutton.classList.add('hidden');
     }else{
-        players = players.slice(0,8);
-        console.log(players.length)
-        console.log('slice')
-    }
-    console.log(players.length)
-    // let count = 0;
+        players = players.slice(0,6);
+        if(players.length < 6){
+            viewAllbutton.classList.add('hidden');
+        }else{
+            viewAllbutton.classList.remove('hidden');
+        };
+    };
 
     const playerInfoContainer = document.getElementById('player-info-container');
     playerInfoContainer.innerHTML = '';
     
     for(let player of players){
         const creatPlayerInfoDiv = document.createElement('div');
-        if(player.strThumb == null){
-            continue;
-        }else{
-            // count++;
+        // if(player.strThumb == null){
+        //     continue;
+        // }else{
             creatPlayerInfoDiv.innerHTML = `
             <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg m-2">
-                <img class="w-full" src="${player.strThumb}" alt="...">
+                <img class="w-full" src="${player.strThumb ? player.strThumb : noImageMsg}" alt="...">
                 <div class="p-1">
                     <h5><span class="font-semibold">Name:</span> ${player.strPlayer}</h5>
                     <p><span class="font-semibold">Sports:</span> ${player.strSport}</p>
@@ -47,36 +47,40 @@ function setPlayerData(players, showAllValues){
                 </div>
             </div>        
             `;
-        }
-        playerInfoContainer.appendChild(creatPlayerInfoDiv);
-        
+        // }
+        playerInfoContainer.appendChild(creatPlayerInfoDiv);  
     };
-    const viewAllbutton = document.getElementById('view-all-btn');
-
     // loading end 
     const loadingDisplay = document.getElementById('loading-display');
     loadingDisplay.classList.add('hidden');
 };
-displayPlayer('');
+   
+displayPlayer(''); /* this function called for auto load some data after getting start */ 
 
+// this function for get the data from input field 
 function searchPlayer(showAllValues){
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
     inputField.value = '';
     displayPlayer(inputFieldValue, showAllValues);
-    // loading strart 
+    // loading spin strart 
     const loadingDisplay = document.getElementById('loading-display');
     loadingDisplay.classList.remove('hidden');
 };
 
-
+/* 
+---------------------details section start ----------------------------------------
+*/
+// this function for get the details data from the API
 function displayPlayerDetails(details){
     fetch(`https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${details}`)
     .then(res => res.json())
     .then(data => setPlayerDetails(data.players[0]))
 };
-
+// this function for view details working section 
 function setPlayerDetails(players){
+    viewAllbutton.classList.add('hidden');
+
     const playerInfoContainer = document.getElementById('player-info-container');
     playerInfoContainer.textContent = '';
 
@@ -84,7 +88,7 @@ function setPlayerDetails(players){
     const creatPlayerDetailsDiv = document.createElement('div');
         creatPlayerDetailsDiv.innerHTML = `
         <div class="grid grid-cols-1 justify-items-center w-3/4 mx-auto border ">
-            <img class="w-fit h-60" src="${players.strThumb}" class=" " alt="...">
+            <img class="w-fit h-60" src="${players.strThumb ? players.strThumb : noImageMsg}" class=" " alt="...">
             <div class="">
                 <div class="flex justify-between my-2">
                     <div>
@@ -102,14 +106,14 @@ function setPlayerDetails(players){
                         <p><span class="font-semibold">Weight:</span> ${players.strWeight}</p>
                     </div>
                 </div>
-                <p><span class="font-semibold">Description:</span> ${players.strDescriptionEN}</p>
+                <p><span class="font-semibold">Description:</span> ${players.strDescriptionEN ? players.strDescriptionEN : 'No description found'}</p>
             </div>
         </div>        
         `;
     playerDetailsContainer.appendChild(creatPlayerDetailsDiv);
 };
-
-document.getElementById('view-all-btn').addEventListener('click', function(){
+// this event handler work for view all button 
+viewAllbutton.addEventListener('click', function(){
         searchPlayer(10);
-})
+});
 
