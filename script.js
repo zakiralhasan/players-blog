cf228dbf6279b6ba520520c6c1f5048193f8ba2f
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-09-01 21:03:39 +0600

    input field validation work

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function setPlayerData(players, showAllValues){
     const playerDetailsContainer = document.getElementById('player-details-container');
     playerDetailsContainer.textContent = '';
     // this condition section works for data slicing and set class for view all button
+    console.log(players)
     if(players.length > 9 && showAllValues){ 
         players = players;
         viewAllbutton.classList.add('hidden');
@@ -32,22 +33,21 @@ function setPlayerData(players, showAllValues){
     playerInfoContainer.innerHTML = '';
     
     for(let player of players){
+        // destructuring 'player' object
+        const {strThumb, strPlayer, strSport, strNationality} = player;
+
         const creatPlayerInfoDiv = document.createElement('div');
-        // if(player.strThumb == null){
-        //     continue;
-        // }else{
             creatPlayerInfoDiv.innerHTML = `
             <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg m-2">
-                <img class="w-full" src="${player.strThumb ? player.strThumb : noImageMsg}" alt="...">
+                <img class="w-full" src="${strThumb ? strThumb : noImageMsg}" alt="...">
                 <div class="p-1">
-                    <h5><span class="font-semibold">Name:</span> ${player.strPlayer}</h5>
-                    <p><span class="font-semibold">Sports:</span> ${player.strSport}</p>
-                    <p><span class="font-semibold">Coundry:</span> ${player.strNationality}</p>
+                    <h5><span class="font-semibold">Name:</span> ${strPlayer ? strPlayer : 'No name found'}</h5>
+                    <p><span class="font-semibold">Sports:</span> ${strSport ? strSport : 'No sports found'}</p>
+                    <p><span class="font-semibold">Coundry:</span> ${strNationality ? strNationality : 'No nationality found'}</p>
                     <button class="bg-blue-400 py-2 mt-2 text-white font-bold w-full" onclick="displayPlayerDetails('${player.idPlayer}')">Details</button>
                 </div>
             </div>        
             `;
-        // }
         playerInfoContainer.appendChild(creatPlayerInfoDiv);  
     };
     // loading end 
@@ -62,7 +62,12 @@ function searchPlayer(showAllValues){
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
     inputField.value = '';
-    displayPlayer(inputFieldValue, showAllValues);
+    console.log(typeof inputFieldValue);
+    if(inputFieldValue === '' || !isNaN(inputFieldValue)){
+        return alert('Please enter players name');
+    }else{
+        displayPlayer(inputFieldValue, showAllValues);
+    };
     // loading spin strart 
     const loadingDisplay = document.getElementById('loading-display');
     loadingDisplay.classList.remove('hidden');
@@ -80,6 +85,8 @@ function displayPlayerDetails(details){
 // this function for view details working section 
 function setPlayerDetails(players){
     viewAllbutton.classList.add('hidden');
+    // destructuring 'players' object 
+    const {strThumb, strPlayer, strSport, strNationality, dateBorn, strBirthLocation, strGender, strHeight, strWeight, strDescriptionEN} = players;
 
     const playerInfoContainer = document.getElementById('player-info-container');
     playerInfoContainer.textContent = '';
@@ -88,25 +95,25 @@ function setPlayerDetails(players){
     const creatPlayerDetailsDiv = document.createElement('div');
         creatPlayerDetailsDiv.innerHTML = `
         <div class="grid grid-cols-1 justify-items-center w-3/4 mx-auto border ">
-            <img class="w-fit h-60" src="${players.strThumb ? players.strThumb : noImageMsg}" class=" " alt="...">
+            <img class="w-fit h-60" src="${strThumb ? strThumb : noImageMsg}" class=" " alt="...">
             <div class="">
                 <div class="flex justify-between my-2">
                     <div>
-                        <h5><span class="font-semibold">Name:</span> ${players.strPlayer}</h5>
-                        <p><span class="font-semibold">Sports:</span> ${players.strSport}</p>
-                        <p><span class="font-semibold">Country:</span> ${players.strNationality}</p>
+                        <h5><span class="font-semibold">Name:</span> ${strPlayer ? strPlayer : 'No name found'}</h5>
+                        <p><span class="font-semibold">Sports:</span> ${strSport ? strSport : 'No sports found'}</p>
+                        <p><span class="font-semibold">Country:</span> ${strNationality ? strNationality : 'No nationality found'}</p>
                     </div>
                     <div>
-                        <p><span class="font-semibold">Date of birth:</span> ${players.dateBorn}</p>
-                        <p><span class="font-semibold">Birth place:</span> ${players.strBirthLocation}</p>
-                        <p><span class="font-semibold">Gender:</span> ${players.strGender}</p>
+                        <p><span class="font-semibold ml-2">Date of birth:</span> ${dateBorn ? dateBorn : 'No Date of birth found'}</p>
+                        <p><span class="font-semibold ml-2">Birth place:</span> ${strBirthLocation ? strBirthLocation : 'No birth place found'}</p>
+                        <p><span class="font-semibold ml-2">Gender:</span> ${strGender ? strGender : 'No gender found'}</p>
                     </div>
                     <div>
-                        <p><span class="font-semibold">Hight:</span> ${players.strHeight}</p>
-                        <p><span class="font-semibold">Weight:</span> ${players.strWeight}</p>
+                        <p><span class="font-semibold ml-2">Hight:</span> ${strHeight ? strHeight : 'No hight found'}</p>
+                        <p><span class="font-semibold ml-2">Weight:</span> ${strWeight ? strWeight : 'No weight found'}</p>
                     </div>
                 </div>
-                <p><span class="font-semibold">Description:</span> ${players.strDescriptionEN ? players.strDescriptionEN : 'No description found'}</p>
+                <p><span class="font-semibold">Description:</span> ${strDescriptionEN ? strDescriptionEN : 'No description found'}</p>
             </div>
         </div>        
         `;
